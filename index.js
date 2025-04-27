7134153bed467effed9b63bef68a21e4a90459a6
Ubuntu <ubuntu@ip-172-31-86-216.ec2.internal>
2025-04-27 06:23:48 +0000

    added

--- index.js
+++ index.js
@@ -1,18 +1,5 @@
-{
-  "name": "SanchariNewRepo",
-  "description": "This is a sample JSON file for Sanchari's project.",
-  "version": "1.0.0",
-  "author": "Sanchari Biswas",
-  "createdAt": "2025-04-27",
-  "data": [
-    {
-      "id": 1,
-      "message": "Hello from JSON!"
-    },
-    {
-      "id": 2,
-      "message": "Good luck, you got this! 🚀"
-    }
-  ]
-}
+const data = require('./index.json');
+
+console.log("Data from JSON file:");
+console.log(data);
 
